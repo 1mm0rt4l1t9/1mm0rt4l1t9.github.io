6808108c631217a110090354fe15c41a85e469a2
Nika Cherepanov <67856658+1mm0rt4l1t9@users.noreply.github.com>
2020-12-19 14:28:54 +0300

    Билдим билд

---
+++ precache-manifest.87346fea700e8744be56a99a125703fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe85d4e887b975fa54bc93ee2430fc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "205259939176b3094e07",
+    "url": "/static/css/main.fca32930.chunk.css"
+  },
+  {
+    "revision": "182b7669da498d011e4c",
+    "url": "/static/js/2.12a6ea2e.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/static/js/2.12a6ea2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "205259939176b3094e07",
+    "url": "/static/js/main.b857b8d9.chunk.js"
+  },
+  {
+    "revision": "a95558b3a99330d88d4d",
+    "url": "/static/js/runtime-main.c5d8ad50.js"
+  }
+]);
